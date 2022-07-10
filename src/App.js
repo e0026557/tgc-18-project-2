638da6ea8331a777079a54ea75f36a3e641bc410
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-07-10 15:15:45 +0000

    Linked style.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/style.css';
+import Main from './main/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<React.Fragment>
+			<Main />
+		</React.Fragment>
+	);
 }
-
-export default App;
