2601f7a4991fb2dfd40c7c8c175f8823ec3e424c
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-07-11 14:41:00 +0000

    Converted navbar in Main.js to component Navigation.js and implemented setActivePage function

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import {Nav, Navbar, Container} from 'react-bootstrap';
+import Navigation from './Navigation';
 
-export default function Main() {
-	return (
-		<React.Fragment>
-			<Navbar className='navbar' fixed="top" bg='light' expand='lg'>
-				<Container>
-					<Navbar.Brand href='#home'>
-						<img className='logo-img' src={require('./../assets/images/coffeetalk.png')} alt="CoffeeTalk Logo" />
-					</Navbar.Brand>
-					<Navbar.Toggle aria-controls='basic-navbar-nav' />
-					<Navbar.Collapse id='basic-navbar-nav'>
-						<Nav className='me-auto'>
-							<Nav.Link className='navbar-link' href='#home'>Home</Nav.Link>
-							<Nav.Link className='navbar-link' href='#recipes'>Recipes</Nav.Link>
-							<Nav.Link className='navbar-link' href='#create'>Create</Nav.Link>
-							<Nav.Link className='navbar-link' href='#favorites'>Favorites</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
-      
-      <div className='section-hero'></div>
-		</React.Fragment>
-	);
+// import { Nav, Navbar, Container } from 'react-bootstrap';
+
+export default class Main extends React.Component {
+	state = {
+		activePage: 'home' // 'home', 'recipes', 'create', and 'favorites'
+	};
+
+	// --- Functions ---
+	setActivePage = (page) => {
+		this.setState({
+			activePage: page
+		});
+	};
+
+	render() {
+		return (
+			<React.Fragment>
+				{/* Navbar */}
+				<Navigation activePage={this.state.activePage} setActivePage={this.setActivePage} />
+
+				{/* Hero Section */}
+				<div className='section-hero'></div>
+
+				{/* Latest 3 Recipes */}
+
+				{/* Top 3 Recipes */}
+			</React.Fragment>
+		);
+	}
 }
---
+++ src/main/Navigation.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Nav, Navbar, Container } from 'react-bootstrap';
+
+export default function Navigation(props) {
+  return (
+    <Navbar className='navbar' fixed='top' bg='light' expand='lg'>
+      <Container>
+        <Navbar.Brand href='#home'>
+          <img
+            className='logo-img'
+            src={require('./../assets/images/logo/coffeetalk.png')}
+            alt='CoffeeTalk Logo'
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link className={props.activePage === 'home' ? 'active' : ''} href='#home' onClick={() => {
+              props.setActivePage('home')
+            }}>
+              Home
+            </Nav.Link>
+            <Nav.Link
+              className={props.activePage === 'recipes' ? 'active' : ''}
+              href='#recipes' onClick={() => {
+                props.setActivePage('recipes')
+              }}
+            >
+              Recipes
+            </Nav.Link>
+            <Nav.Link
+              className={props.activePage === 'create' ? 'active' : ''}
+              href='#create' onClick={() => {
+                props.setActivePage('create')
+              }}
+            >
+              Create
+            </Nav.Link>
+            <Nav.Link
+              className={props.activePage === 'favorites' ? 'active' : ''}
+              href='#favorites' onClick={() => {
+                props.setActivePage('favorites')
+              }}
+            >
+              Favorites
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
