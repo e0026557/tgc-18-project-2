05aabed13a33d6d5a3fef0375efac71e878b1e4b
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-07-13 06:35:14 +0000

    Added hero section content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
-  return (
-    <React.Fragment>
-      {/* Hero Section */}
-      <div className='section-hero adjust-margin-top'></div>
+	return (
+		<React.Fragment>
+			{/* Hero Section */}
+			<div className='section-hero adjust-margin-top'>
+				<div className='hero-box'>
+					<h1 className='hero-header'>CoffeeTalk</h1>
+					<span className='hero-span'>Coffee-making made easier</span>
+				</div>
+				<FontAwesomeIcon icon={faChevronDown} />
+			</div>
 
-      {/* Latest 3 Recipes */}
+			{/* Latest 3 Recipes */}
 
-      {/* Top 3 Recipes */}
-    </React.Fragment>
-  )
+			{/* Top 3 Recipes */}
+		</React.Fragment>
+	);
 }
