c4eba68776f4946a3397839d67d60f21f0644274
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-07-13 08:00:48 +0000

    Added LoadingSpinner component and its corresponding CSS

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function LoadingSpinner() {
+  return (
+    <div class="spinner">
+      <div class="bounce1"></div>
+      <div class="bounce2"></div>
+      <div class="bounce3"></div>
+    </div>
+  )
+}
