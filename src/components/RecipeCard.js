b6ad3326e9ecc30a489b800587926d922ff279fa
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-07-13 10:17:20 +0000

    Populate recipe information in Recipe Card component and shifted button outside of card body

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -4,15 +4,12 @@ import Card from 'react-bootstrap/Card';
 export default function RecipeCard(props) {
 	return (
 		<Card className='recipe-card mt-3'>
-			<Card.Img variant='top' src='holder.js/100px180' />
+			<Card.Img variant='top' src={props.recipe.image_url} />
 			<Card.Body>
-				<Card.Title>Card Title</Card.Title>
-				<Card.Text>
-					Some quick example text to build on the card title and make
-					up the bulk of the card's content.
-				</Card.Text>
-				<Button variant='primary'>Go somewhere</Button>
+				<Card.Title>{props.recipe.recipe_name}</Card.Title>
+				<Card.Text>{props.recipe.description}</Card.Text>
 			</Card.Body>
+			<Button variant='primary'>More info</Button>
 		</Card>
 	);
 }
