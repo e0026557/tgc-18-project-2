4d81b9df2ef9ee3991c7e254c5672db75d30db0b
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-07-13 15:12:31 +0000

    Added AddToFavoritesModal component and implemented in RecipeCard.js when bookmark button is clicked

---
+++ src/components/EmailModal.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
+export default class AddToFavoritesModal extends React.Component {
+	// --- State ----
+	state = {
+		email: ''
+	};
+
+	// --- Functions ---
+	updateFormField = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
+
+	render() {
+		return (
+			<Modal
+				{...this.props}
+				size='lg'
+				aria-labelledby='contained-modal-title-vcenter'
+				centered
+			>
+				<Modal.Header closeButton>
+					<Modal.Title id='contained-modal-title-vcenter'>
+						Add to Favorites
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form.Control
+						className='mt-4 mb-3'
+						name='email'
+						type='email'
+						placeholder='Enter email'
+						value={this.state.email}
+						onChange={this.updateFormField}
+					/>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant='secondary' onClick={this.props.onHide}>
+						Close
+					</Button>
+					<Button variant='primary' onClick={this.props.onHide}>
+						Add
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		);
+	}
+}
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,22 +1,36 @@
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import AddToFavoritesModal from './EmailModal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeartCirclePlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function RecipeCard(props) {
+	const [modalShow, setModalShow] = React.useState(false);
+	
 	return (
-		<Card className='recipe-card mt-3'>
-			<Card.Img variant='top' src={props.recipe.image_url} />
-			<Card.Body>
-				<div className='d-flex justify-content-between align-items-start'>
-					<Card.Title>{props.recipe.recipe_name}</Card.Title>
-					<a className='btn btn-sm card-btn-bookmark'>
-						<FontAwesomeIcon icon={faHeartCirclePlus} />
-					</a>
-				</div>
-				<Card.Text>{props.recipe.description}</Card.Text>
-			</Card.Body>
-			<Button className='card-btn-info mt-2' variant='primary'>More info</Button>
-		</Card>
+		<React.Fragment>
+			{/* Recipe card */}
+			<Card className='recipe-card mt-3'>
+				<Card.Img variant='top' src={props.recipe.image_url} />
+				<Card.Body>
+					<div className='d-flex justify-content-between align-items-start'>
+						<Card.Title>{props.recipe.recipe_name}</Card.Title>
+						<a className='btn btn-sm card-btn-bookmark' onClick={() => setModalShow(true)}>
+							<FontAwesomeIcon icon={faHeartCirclePlus} />
+						</a>
+					</div>
+					<Card.Text>{props.recipe.description}</Card.Text>
+				</Card.Body>
+				<Button className='card-btn-info mt-2' variant='primary'>More info</Button>
+			</Card>
+
+			{/* Email Modal for adding to Favorites collection */}
+			<AddToFavoritesModal
+				show={modalShow}
+				onHide={() => setModalShow(false)}
+			/>
+		</React.Fragment>
 	);
 }
+
