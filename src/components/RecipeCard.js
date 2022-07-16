acff3b824fe2cb4ccd1f12bdef4c3c4366f3fd79
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-07-16 11:22:03 +0000

    Converted RecipeCard from function-based component to class-based component

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -5,43 +5,53 @@ import AddToFavoritesModal from './AddToFavoritesModal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeartCirclePlus } from '@fortawesome/free-solid-svg-icons';
 
-export default function RecipeCard(props) {
-	const [modalShow, setModalShow] = React.useState(false);
+export default class RecipeCard extends React.Component {
+	// const[modalShow, setModalShow] = React.useState(false);
+	state = {
+		modalShow: false,
+	}
 
-	return (
-		<React.Fragment>
-			{/* Recipe card */}
-			<Card className='recipe-card mt-3'>
-				<Card.Img variant='top' src={props.recipe.image_url} />
-				<Card.Body>
-					<div className='d-flex justify-content-between align-items-start'>
-						<Card.Title>{props.recipe.recipe_name}</Card.Title>
-						<Button
-							className='btn btn-sm card-btn-bookmark'
-							onClick={() => setModalShow(true)}
-						>
-							<FontAwesomeIcon icon={faHeartCirclePlus} />
-						</Button>
-					</div>
-					<Card.Text>{props.recipe.description}</Card.Text>
-				</Card.Body>
-				<Button
-					className='card-btn-info mt-2'
-					variant='primary'
-					onClick={() => {
-						props.setActivePage('recipe');
-					}}
-				>
-					More info
-				</Button>
-			</Card>
+	render() {
+		return (
+			<React.Fragment>
+				{/* Recipe card */}
+				<Card className='recipe-card mt-3'>
+					<Card.Img variant='top' src={this.props.recipe.image_url} />
+					<Card.Body>
+						<div className='d-flex justify-content-between align-items-start'>
+							<Card.Title>{this.props.recipe.recipe_name}</Card.Title>
+							<Button
+								className='btn btn-sm card-btn-bookmark'
+								onClick={() => this.setState({
+									modalShow: true
+								})}
+							>
+								<FontAwesomeIcon icon={faHeartCirclePlus} />
+							</Button>
+						</div>
+						<Card.Text>{this.props.recipe.description}</Card.Text>
+					</Card.Body>
+					<Button
+						className='card-btn-info mt-2'
+						variant='primary'
+						onClick={() => {
+							this.props.setActivePage('recipe');
+						}}
+					>
+						More info
+					</Button>
+				</Card>
 
-			{/* Email Modal for adding to Favorites collection */}
-			<AddToFavoritesModal
-				recipeid={props.recipe._id}
-				show={modalShow}
-				onHide={() => setModalShow(false)}
-			/>
-		</React.Fragment>
-	);
+				{/* Email Modal for adding to Favorites collection */}
+				<AddToFavoritesModal
+					recipeid={this.props.recipe._id}
+					show={this.state.modalShow}
+					onHide={() => this.setState({
+						modalShow: false
+					})}
+				/>
+			</React.Fragment>
+		);
+	}
+
 }
