a6a140df6946a1c89e3eca1f4f866466e2646464
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-07-17 03:31:17 +0000

    Added onClick function to logo in navbar to go to homepage

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,41 +5,65 @@ export default function Navigation(props) {
   return (
     <Navbar className='navbar' fixed='top' bg='light' expand='lg'>
       <Container fluid>
-        <Navbar.Brand className='ms-3' href='#home'>
+        <Navbar.Brand
+          className='ms-3'
+          href='#home'
+          onClick={() => {
+            props.setActivePage('home');
+          }}
+        >
           <img
             className='logo-img'
             src={require('./../assets/images/logo/coffeetalk.png')}
             alt='CoffeeTalk Logo'
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' className='me-3' />
+        <Navbar.Toggle
+          aria-controls='basic-navbar-nav'
+          className='me-3'
+        />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
-            <Nav.Link className={props.activePage === 'home' ? 'active' : ''} href='#home' onClick={() => {
-              props.setActivePage('home')
-            }}>
+            <Nav.Link
+              className={
+                props.activePage === 'home' ? 'active' : ''
+              }
+              href='#home'
+              onClick={() => {
+                props.setActivePage('home');
+              }}
+            >
               Home
             </Nav.Link>
             <Nav.Link
-              className={props.activePage === 'recipes' ? 'active' : ''}
-              href='#recipes' onClick={() => {
-                props.setActivePage('recipes')
+              className={
+                props.activePage === 'recipes' ? 'active' : ''
+              }
+              href='#recipes'
+              onClick={() => {
+                props.setActivePage('recipes');
               }}
             >
               Recipes
             </Nav.Link>
             <Nav.Link
-              className={props.activePage === 'create' ? 'active' : ''}
-              href='#create' onClick={() => {
-                props.setActivePage('create')
+              className={
+                props.activePage === 'create' ? 'active' : ''
+              }
+              href='#create'
+              onClick={() => {
+                props.setActivePage('create');
               }}
             >
               Create
             </Nav.Link>
             <Nav.Link
-              className={props.activePage === 'favorites' ? 'active' : ''}
-              href='#favorites' onClick={() => {
-                props.setActivePage('favorites')
+              className={
+                props.activePage === 'favorites' ? 'active' : ''
+              }
+              href='#favorites'
+              onClick={() => {
+                props.setActivePage('favorites');
               }}
             >
               Favorites
@@ -48,5 +72,5 @@ export default function Navigation(props) {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
+  );
 }
